99bb8ded97552bbb7a9d4c19b645777898bd4ce7
glaubert <suyann12@hotmail.com>
2024-05-09 20:12:39 -0300

    :sparkles: getall and getuser method

---
+++ README.md
@@ -0,0 +1,16 @@
+# Standard Crud Api 
+
+
+### API_ROOT
+```
+settings.py # Configs gerais
+urls.py # Linkar aplicação
+```
+
+
+### API_REST
+models.py # criar modelos para o banco dedados
+admin.py # Configurar edição de modelos (opcional)
+serializers.py # Generar JSON's dos modelos
+views.py # Funções API
+urls.py # Linkar funções
---
+++ api_rest/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json
+
+@api_view(['GET'])
+def get_users(request):
+
+    if request.method == 'GET':
+
+        users = User.objects.all()                          # Get all objects in User's database (It returns a queryset)
+
+        serializer = UserSerializer(users, many=True)       # Serialize the object data into json (Has a 'many' parameter cause it's a queryset)
+
+        return Response(serializer.data)                    # Return the serialized data
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def get_by_nick(request, nick):
+
+    try:
+        user = User.objects.get(pk=nick)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if(request.method == 'GET'):
+        
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
