3e6d15b5cdc4a37a41bb6134ec23a77653008edc
glaubert <suyann12@hotmail.com>
2024-05-10 16:58:29 -0300

    :sparkles: update and delete methods

--- api_rest/views.py
+++ api_rest/views.py
@@ -24,7 +24,8 @@ def get_users(request):
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+
+@api_view(['GET', 'PUT'])
 def get_by_nick(request, nick):
 
     try:
@@ -32,8 +33,84 @@ def get_by_nick(request, nick):
     except:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
+
+
     if(request.method == 'GET'):
-        
         serializer = UserSerializer(user)
         return Response(serializer.data)
     
+    if(request.method == 'PUT'):
+        serializer = UserSerializer(user, data=request.data)
+
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_manager(request):
+
+    ### SHOW ACCOUNTS
+    if(request.method == 'GET'):
+
+        try:
+            if(request.GET['user']):
+                user_nickname = request.GET['user']
+
+                try:
+                    user = User.objects.get(pk=user_nickname)
+                except:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+                
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+
+    ### CREATE ACCOUNTS
+    if(request.method == 'POST'):
+
+        new_user = request.data
+
+        serializer = UserSerializer(data=new_user)
+        
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+            
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+
+    ### CREATE ACCOUNTS
+    if(request.method == 'PUT'):
+
+        nickname = request.data['user_nickname']
+        
+        try:
+            update_user = User.objects.get(pk=nickname)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = UserSerializer(update_user, data=request.data)
+
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(status=status.HTTP_202_ACCEPTED)
+         
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+
+    if(request.method == 'DELETE'):
+        
+        try:
+            user_to_delete = User.objects.get(pk=request.data['user_nickname'])
+            user_to_delete.delete()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
